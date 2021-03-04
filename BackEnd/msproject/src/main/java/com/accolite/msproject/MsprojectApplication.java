684401fe8cdea080b908961c3edb0205b5001b17
Jay <jayparthshah786@gmail.com>
2021-03-04 21:05:27 +0530

    initial setup done

---
+++ BackEnd/msproject/src/main/java/com/accolite/msproject/MsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.accolite.msproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprojectApplication.class, args);
+	}
+
+}
